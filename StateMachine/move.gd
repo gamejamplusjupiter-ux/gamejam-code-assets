6aac78d96afe726e243ea749f5de79b639c02e48
PedroCaurio <cauriopedro2@gmail.com>
2025-10-16 18:38:01 -0300

    add: place holder assets and state machine logic

---
+++ StateMachine/move.gd
@@ -0,0 +1,29 @@
+extends State
+
+
+@export var jump_state: State
+
+@export var idle_state: State
+
+
+func enter() -> void:
+	super()
+	
+	
+func process_input(event: InputEvent) -> State:
+	if Input.is_action_just_pressed("jump") and parent.is_on_floor():
+		return jump_state
+	return null
+	
+func process_physics(delta: float) -> State:
+	var movement = Input.get_axis("move_left", "move_right") * move_speed
+	
+	
+	if movement == 0.0:
+		print(movement)
+		return idle_state
+	parent.animation.flip_h = movement < 0
+	parent.velocity.x = movement
+	parent.move_and_slide()
+	
+	return null
---
+++ StateMachine/player.gd
@@ -0,0 +1,19 @@
+class_name Player
+extends CharacterBody2D
+
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+@onready var state_machine: Node = $StateMachine
+@onready var state: Label = $State
+
+func _ready() -> void:
+	state_machine.init(self)
+	
+func _unhandled_input(event: InputEvent) -> void:
+	state_machine.process_input(event)
+	
+func _physics_process(delta: float) -> void:
+	state_machine.process_physics(delta)
+	
+func _process(delta: float) -> void:
+	state_machine.process_frame(delta)
+	state.text = state_machine.current_state.name
