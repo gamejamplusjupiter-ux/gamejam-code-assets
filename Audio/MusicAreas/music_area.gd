87ad34760ac54504891602f5bc54caa4f7feed66
Debora Carvalho <dbndy3@gmail.com>
2025-10-16 21:01:53 -0300

    Implementa AudioManager e limpa arquivos desnecessários

---
+++ Audio/MusicAreas/music_area.gd
@@ -0,0 +1,26 @@
+extends Area2D
+class_name MusicArea
+
+@export var track : AudioStream # the track that should be played in this area
+
+func _on_body_entered(body: Node2D) -> void: 
+	if body is Player:
+		if body.music_area_array.is_empty(): # No music is currently playing
+			if track:
+				AudioManager.fade_music_in(track)
+		else: # some music is already playing
+			if track:
+				AudioManager.crossfade_music_to(track)
+			
+		body.music_area_array.append(self)
+
+func _on_body_exited(body: Node2D) -> void: 
+	if body is Player: 
+		body.music_area_array.erase(self)
+		
+		if !body.music_area_array.is_empty(): # music was stacked
+			var last_track : AudioStream = body.music_area_array[-1].track
+			if last_track:
+				AudioManager.crossfade_music_to(last_track)
+		else: # no music stacked
+			AudioManager.fade_music_out()
